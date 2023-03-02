d0e1308d004bb26780e87cc9750ae70e8d16bb10
Lazy <117997202+Lazy14@users.noreply.github.com>
2023-03-02 18:46:00 +0200

    Cpp

--- Lab 1 oop.cpp
+++ Lab 1 oop.cpp
@@ -1,20 +1,60 @@
 ﻿
 
 #include <iostream>
+#include <string>   //підклучаємо базу данних string
 
 using namespace std;
 
-class MyClass
+class Animals  // створуємо структуру "тварини"
 {
-public:
-	int a = 10;
+private:
+    string name, kind; //створуємо поля "імя" "вид" типу "string"
+	int average_weight; //створуємо поле "середня вага" типу "integer"
 
+
+public:
+
+	Animals(string N, string k, int av)
+	{
+		name = N;
+		kind = k;
+		average_weight = av;
+		
+
+	}
+
+	~Animals() {
+
+	}
+	
+	string GetName() {
+		return name;
+	}
+	string GetKind() {
+		return kind;
+	}
+	int GetAverage_weight() {
+		return average_weight;
+	}
+
+	void Print() 
+	{
+		cout << endl << "\tIмя: " << name << endl << "\tВид: " << kind << endl <<"\tСередня вага: " << average_weight <<"кг" << endl;
+	}
+	
 };
 
 int main() {
+	setlocale(LC_ALL, "ukr");
 
-	MyClass m;
-	cout << m.a;
+	Animals tiger("Тигр", "Тигр", 70);
+	Animals cheetah("Гепард", "Гепард", 50);
+	Animals hare("Заєць", "Заєць", 3);
+	
+	tiger.Print();
+	cheetah.Print();
+	hare.Print();
 
+	return 0;
 }
 
